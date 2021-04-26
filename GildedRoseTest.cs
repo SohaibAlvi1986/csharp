91bb0963d0e172f4c86221e5465ddbaf092e11d2
Sohaib Alvi <sohaibalvi.ccm@gmail.com>
2021-04-26 16:53:53 +0200

    Refectoring UpdateQuality fucntion

--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -11,7 +11,7 @@ namespace csharp
         {
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
+            app.UpdateQuality();   
             Assert.AreEqual("foo", Items[0].Name);
         }
     }
