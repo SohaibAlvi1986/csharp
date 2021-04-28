28791323151fdbcdd012d8bea2a54eba9128e537
Sohaib Alvi <sohaibalvi.ccm@gmail.com>
2021-04-28 04:51:09 +0200

    Remove If Else statments from code

--- Item.cs
+++ Item.cs
@@ -1,24 +1,170 @@
 ﻿namespace csharp
-{
+{  
+
     public class Item
     {
+        interface IsAgedBrie
+        {
+            bool isYes();
+            void handleExpired();
+            bool notBrieAndNotBackstagePasses();
+            void updateQuality();
+        }
+        public class AgedBrie : IsAgedBrie
+        {
+            private Item itm;
+            public AgedBrie(Item _itm)
+            {
+                itm = _itm;
+            }
+            public bool isYes()
+            {
+                return true;
+            }
+            public void handleExpired()
+            {
+                itm.increaseQualityIfNotMax();
+            }
+
+            public bool notBrieAndNotBackstagePasses()
+            {
+                return false;
+            }
+            public void updateQuality()
+            {
+                itm.increaseQualityIncludingBackstagePasses();
+            }
+        }
+        public class NotAgedBrie : IsAgedBrie
+        {
+            private Item itm;
+            public NotAgedBrie(Item _itm)
+            {
+                itm = _itm;
+            }
+            public bool isYes()
+            {
+                return false;
+            }
+            public void handleExpired()
+            {
+                itm.handleExpiredItemNotAgedBrie();
+            }
+
+            public bool notBrieAndNotBackstagePasses()
+            {
+                return !(itm.isBackstagePasses.isYes());
+            }
+            public void updateQuality()
+            {
+                itm.isBackstagePasses.updateQuality();
+            }
+        }     
+        
+        interface IsBackstagePasses
+        {
+            bool isYes();
+            void handleExpiredItemNotAgedBrie();
+
+            void updateQuality();
+        }
+
+        class BackstagePasses : IsBackstagePasses
+        {
+            private Item itm;
+            public BackstagePasses(Item _itm)
+            {
+                itm = _itm;
+            }
+            public bool isYes()
+            {
+                return true;
+            }
+
+            public void handleExpiredItemNotAgedBrie()
+            {
+                itm.Quality = 0;
+            }
+
+            public void updateQuality()
+            {
+                itm.increaseQualityIncludingBackstagePasses();
+            }
+        }
+
+        class NotBackstagePasses : IsBackstagePasses
+        {
+            private Item itm;
+
+            public NotBackstagePasses(Item _itm)
+            {
+                itm = _itm;
+            }
+            public bool isYes()
+            {
+                return false;
+            }
+
+            public void handleExpiredItemNotAgedBrie()
+            {
+                itm.decreaseQualityIfItemHasQuality();
+            }
+
+            public void updateQuality()
+            {
+                itm.decreaseQualityIfItemHasQuality();
+            }
+        }
+
+
         public string Name { get; set; }
         public int SellIn { get; set; }
         public int Quality { get; set; }
+
+        private IsAgedBrie isAgedBrie;
+
+        private IsBackstagePasses isBackstagePasses;
+
+        private bool isSulfuras = false;
+
+        private const string AGED_BRIE = "Aged Brie";
+
+        private const string BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
+
+        private const string SULFURAS = "Sulfuras, Hand of Ragnaros";
+
+        private const int MAX_QUALITY = 50;
+
+        private const int FAR_FROM_EXPIRY = 11;
+
+        private const int CLOSED_TO_EXPIRY = 6;
 
         public override string ToString()
         {
             return this.Name + ", " + this.SellIn + ", " + this.Quality;
         }
 
+        public Item()
+        {
+            isAgedBrie = Name.Equals(AGED_BRIE) ? new AgedBrie(this) as IsAgedBrie : 
+                                                  new NotAgedBrie(this) as IsAgedBrie;
+
+            isBackstagePasses = this.Name.Equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT) ?
+                                                  new BackstagePasses(this) as IsBackstagePasses :
+                                                  new NotBackstagePasses(this) as IsBackstagePasses;
+
+            isSulfuras = this.Name.Equals(SULFURAS);
+        }
+
         public void increaseQuality()
         {
-            this.Quality = this.Quality + 1;
+            if (this.Quality < MAX_QUALITY)
+                this.Quality++;
         }
 
         public void increaseQualityIfNotMax()
         {
-            if (this.Quality < 50)
+            if (this.Quality < MAX_QUALITY)
             {
                 this.increaseQuality();
             }
@@ -26,7 +172,7 @@
 
         public void increaseQualityIfFarFromExpiry()
         {
-            if (this.SellIn < 11)
+            if (this.SellIn < FAR_FROM_EXPIRY)
             {
                 this.increaseQualityIfNotMax();
             }
@@ -34,7 +180,7 @@
 
         public void increaseQualityIfCloseToExpiry()
         {
-            if (this.SellIn < 6)
+            if (this.SellIn < CLOSED_TO_EXPIRY)
             {
                 this.increaseQualityIfNotMax();
             }
@@ -42,7 +188,7 @@
 
         public void increaseQualityOfBackstagePasses()
         {
-            if (!this.Name.Equals("Backstage passes to a TAFKAL80ETC concert"))
+            if (isBackstagePasses.isYes())
             {
                 this.increaseQualityIfFarFromExpiry();
                 this.increaseQualityIfCloseToExpiry();
@@ -51,7 +197,7 @@
 
         public void increaseQualityIncludingBackstagePasses()
         {
-            if (this.Quality < 50)
+            if (this.Quality < MAX_QUALITY)
             {
                 this.increaseQuality();
                 this.increaseQualityOfBackstagePasses();
@@ -60,9 +206,9 @@
 
         public void decreaseQuality()
         {
-            if (!this.Name.Equals("Sulfuras, Hand of Ragnaros"))
+            if (!isSulfuras)
             {
-                this.Quality = this.Quality - 1;
+                this.Quality--;
             }
         }
 
@@ -74,55 +220,39 @@
             }
         }
 
-        public void updateQuality()
+        private void updateQuality()
         {
-            if (!this.Name.Equals("Aged Brie") && !this.Name.Equals("Backstage passes to a TAFKAL80ETC concert"))
-            {
-                this.decreaseQualityIfItemHasQuality();
-            }
-            else
-            {
-                this.increaseQualityIncludingBackstagePasses();
-            }
+            isAgedBrie.updateQuality();
+        }
+
+        private bool notBrieAndNotBackstagePasses()
+        {
+            return isAgedBrie.notBrieAndNotBackstagePasses();
         }
 
         public void handleExpiredItemNotAgedBrie()
         {
-            if (!this.Name.Equals("Backstage passes to a TAFKAL80ETC concert"))
-            {
-                this.decreaseQualityIfItemHasQuality();
-            }
-            else
-            {
-                this.Quality = this.Quality - this.Quality;
-            }
+            isBackstagePasses.handleExpiredItemNotAgedBrie();
         }
 
         public void handleExpired()
         {
-            if (!this.Name.Equals("Aged Brie"))
-            {
-                this.handleExpiredItemNotAgedBrie();
-            }
-            else
-            {
-                this.increaseQualityIfNotMax();
-            }
+            isAgedBrie.handleExpired();
         }
 
         public void handleIfExpired()
         {
             if (this.SellIn < 0)
             {
-                this.handleExpired();
+                isAgedBrie.handleExpired();
             }
         }
 
         public void updateSellIn()
         {
-            if (this.Name != "Sulfuras, Hand of Ragnaros")
+            if (!isSulfuras)
             {
-                this.SellIn = this.SellIn - 1;
+                this.SellIn--;
             }
 
             this.handleIfExpired();
